cd73714abcf9121a15016aafb309bb13c429fa3b
Vaibhav Bedia <vaibhav.bedia@ti.com>
2012-12-31 10:57:25 +0530

    ARM: OMAP2+: AM33XX: Basic suspend resume support
    AM335x supports various low power modes as documented
    in section 8.1.4.3 of the AM335x TRM which is available
    @ http://www.ti.com/litv/pdf/spruh73f
    
    DeepSleep0 mode offers the lowest power mode with limited
    wakeup sources without a system reboot and is mapped as
    the suspend state in the kernel. In this state, MPU and
    PER domains are turned off with the internal RAM held in
    retention to facilitate resume process. As part of the boot
    process, the assembly code is copied over to OCMCRAM using
    the OMAP SRAM code.
    
    AM335x has a Cortex-M3 (WKUP_M3) which assists the MPU
    in DeepSleep0 entry and exit. WKUP_M3 takes care of the
    clockdomain and powerdomain transitions based on the
    intended low power state. MPU needs to load the appropriate
    WKUP_M3 binary onto the WKUP_M3 memory space before it can
    leverage any of the PM features like DeepSleep.
    
    The IPC mechanism between MPU and WKUP_M3 uses a mailbox
    sub-module and 8 IPC registers in the Control module. MPU
    uses the assigned Mailbox for issuing an interrupt to
    WKUP_M3 which then goes and checks the IPC registers for
    the payload. WKUP_M3 has the ability to trigger on interrupt
    to MPU by executing the "sev" instruction.
    
    In the current implementation when the suspend process
    is initiated MPU interrupts the WKUP_M3 to let it know about
    the intent of entering DeepSleep0 and waits for an ACK. When
    the ACK is received MPU continues with its suspend process
    to suspend all the drivers and then jumps to assembly in
    OCMC RAM. The assembly code puts the PLLs in bypass, puts the
    external RAM in self-refresh mode and then finally execute the
    WFI instruction. Execution of the WFI instruction triggers another
    interrupt to the WKUP_M3 which then continues wiht the power down
    sequence wherein the clockdomain and powerdomain transition takes
    place. As part of the sleep sequence, WKUP_M3 unmasks the interrupt
    lines for the wakeup sources. WFI execution on WKUP_M3 causes the
    hardware to disable the main oscillator of the SoC.
    
    When a wakeup event occurs, WKUP_M3 starts the power-up
    sequence by switching on the power domains and finally
    enabling the clock to MPU. Since the MPU gets powered down
    as part of the sleep sequence in the resume path ROM code
    starts executing. The ROM code detects a wakeup from sleep
    and then jumps to the resume location in OCMC which was
    populated in one of the IPC registers as part of the suspend
    sequence.
    
    The low level code in OCMC relocks the PLLs, enables access
    to external RAM and then jumps to the cpu_resume code of
    the kernel to finish the resume process.
    
    Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>
    Cc: Tony Lingren <tony@atomide.com>
    Cc: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Cc: Benoit Cousson <b-cousson@ti.com>
    Cc: Paul Walmsley <paul@pwsan.com>
    Cc: Kevin Hilman <khilman@deeprootsystems.com>

---
+++ arch/arm/mach-omap2/pm33xx.h
@@ -0,0 +1,76 @@
+/*
+ * AM33XX Power Management Routines
+ *
+ * Copyright (C) 2012 Texas Instruments Inc.
+ * Vaibhav Bedia <vaibhav.bedia@ti.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __ARCH_ARM_MACH_OMAP2_PM33XX_H
+#define __ARCH_ARM_MACH_OMAP2_PM33XX_H
+
+#include "control.h"
+
+#ifndef __ASSEMBLER__
+
+/*
+ * This enum is used to index the array passed to suspend routine with
+ * parameters that vary across DDR2 and DDR3 sleep sequence.
+ *
+ * Since these are used to load into registers by suspend code,
+ * entries here must always be in sync with the suspend code
+ * in arm/mach-omap2/sleep33xx.S
+ */
+enum suspend_cfg_params {
+	EMIF_ADDR_VIRT = 0,
+	MEMORY_TYPE,
+	SUSPEND_CFG_PARAMS_END /* Must always be the last entry */
+};
+
+struct wkup_m3_context {
+	struct am33xx_ipc_data	ipc_data;
+	struct device		*dev;
+	struct firmware		*firmware;
+	struct mailbox		*mbox;
+	void __iomem		*code;
+	u8			state;
+	u32			version;
+};
+
+#ifdef CONFIG_SUSPEND
+static void am33xx_m3_state_machine_reset(void);
+#else
+static inline void am33xx_m3_state_machine_reset(void) {}
+#endif /* CONFIG_SUSPEND */
+
+extern void __iomem *am33xx_get_emif_base(void);
+#endif
+
+#define	IPC_CMD_DS0			0x3
+#define IPC_CMD_RESET                   0xe
+#define DS_IPC_DEFAULT			0xffffffff
+#define M3_VERSION_UNKNOWN		0x0000ffff
+#define M3_BASELINE_VERSION		0x20
+
+#define IPC_RESP_SHIFT			16
+#define IPC_RESP_MASK			(0xffff << 16)
+
+#define M3_STATE_UNKNOWN		0
+#define M3_STATE_RESET			1
+#define M3_STATE_INITED			2
+#define M3_STATE_MSG_FOR_LP		3
+#define M3_STATE_MSG_FOR_RESET		4
+
+#define AM33XX_OCMC_END			0x40310000
+#define AM33XX_EMIF_BASE		0x4C000000
+
+#define MEM_TYPE_DDR2		2
+
+#endif
