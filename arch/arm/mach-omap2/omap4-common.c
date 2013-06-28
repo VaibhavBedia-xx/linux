92eff5dd9b25a11890b33d10d6c999c7ae69d215
Vaibhav Bedia <vaibhav.bedia@ti.com>
2013-06-28 14:22:11 +0530

    ARM: OMAP2+: AM33XX: Get rid of custom PRM, CM APIs
    Now that we have migrated AM335x over to use OMAP4 style
    PRM, CM APIs we can delete the custom APIs
    
    To avoid build breakage the reset function is reimplemented
    in the same patch.
    
    Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>

--- arch/arm/mach-omap2/omap4-common.c
+++ arch/arm/mach-omap2/omap4-common.c
@@ -355,3 +355,17 @@ void omap44xx_restart(char mode, const char *cmd)
 	while (1);
 }
 
+/**
+ * omap44xx_restart - trigger a software restart of the SoC
+ * @mode: the "reboot mode", see arch/arm/kernel/{setup,process}.c
+ * @cmd: passed from the userspace program rebooting the system (if provided)
+ *
+ * Resets the SoC.  For @cmd, see the 'reboot' syscall in
+ * kernel/sys.c.  No return value.
+ */
+void am33xx_restart(char mode, const char *cmd)
+{
+	/* XXX Should save 'cmd' into scratchpad for use after reboot */
+	am33xx_prminst_global_warm_sw_reset(); /* never returns */
+	while (1);
+}
