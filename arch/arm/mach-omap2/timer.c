cc2e99fdc0bb8995849bff85df8c3daf71b24dc8
Vaibhav Bedia <vaibhav.bedia@ti.com>
2013-07-19 14:59:48 +0530

    ARM: OMAP2+: timer: Change AM43XX timer name to avoid conflict with OMAP3
    Current name for AM43XX timer leads to conflict in multi-OMAP
    builds.
    
    Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>

--- arch/arm/mach-omap2/timer.c
+++ arch/arm/mach-omap2/timer.c
@@ -622,9 +622,9 @@ OMAP_SYS_32K_TIMER_INIT(3_secure, 12, "secure_32k_fck", "ti,timer-secure",
 #endif /* CONFIG_ARCH_OMAP3 */
 
 #if defined(CONFIG_SOC_AM43XX)
-OMAP_SYS_32K_TIMER_INIT(3, 1, "timer_sys_ck", "ti,timer-alwon",
+OMAP_SYS_32K_TIMER_INIT(43, 1, "timer_sys_ck", "ti,timer-alwon",
 			2, "timer_sys_ck", NULL);
-#endif 
+#endif
 
 #if defined(CONFIG_ARCH_OMAP3) || defined(CONFIG_SOC_AM33XX)
 OMAP_SYS_GP_TIMER_INIT(3, 2, "timer_sys_ck", NULL,
